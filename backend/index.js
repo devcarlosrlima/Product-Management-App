e0359d1414d22ad6a76e5195adfbfd1800a41361
Carlos Lima <136191341+devcarlosrlima@users.noreply.github.com>
2024-06-26 07:45:04 -0300

    atualização do back

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,24 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+
+
 const app = express();
-const port = 3002; 
+const port =  process.env.PORT || 3002; 
 
 app.use(bodyParser.json());
 app.use(cors());
 
 // String de conexão MongoDB
-const mongoUri = 'mongodb://localhost:27017/productdb';
+const mongoUri = process.env.MONGO_URI;
+
+if (!mongoUri) {
+  throw new Error('Missing MONGO_URI environment variable');
+}
 
 // Conectar ao MongoDB
 mongoose.connect(mongoUri)
